8e7adf76746b6fa930a160b00d9cb27fe7cfa655
KhushiAgg3404 <aggarwalkhushi2025@gmail.com>
2026-06-03 00:23:24 +0530

    Use Render PORT environment variable

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -33,7 +33,8 @@ app.get("/:id",redirectFromShortUrl)
 
 app.use(errorHandler)
 
-app.listen(3000,()=>{
-    connectDB()
-    console.log("Server is running on http://localhost:3000");
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
